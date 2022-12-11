9f58567fed4530e1da7375dfb16f394ba3a8a74c
Ari <ari.khaytser@gmail.com>
2022-12-10 21:15:08 -0500

    Initial Commit

---
+++ robot_image_dataset.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+class Robot_Image_Dataset(Dataset):
+    def __init__(self, X, Y=None):
+        
+
+    def __len__(self):
+
+    def transform(self):
+
+    def __getitem__(self, idx):
